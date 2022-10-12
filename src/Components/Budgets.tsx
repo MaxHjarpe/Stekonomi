a8f3351860ec238cd9cba2f272ddfd6cecfa1ae0
Anton Karlsson <Anton7karlsson@gmail.com>
2022-10-12 12:08:30 +0200

    budgets

---
+++ src/Components/Budgets.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Budgets = () => {
+    return (
+        <div className="BudgetButtons">
+            <button className="Button1">Namn, 2022-07-22</button>
+            <button className="Button2">Namn2, 2022-08-22</button>
+            <button className="Button3">Namn3, 2022-09-22</button>
+        </div>
+    )
+}  
+
+export default Budgets;
