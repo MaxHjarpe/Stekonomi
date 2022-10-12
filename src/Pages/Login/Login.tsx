601dfcc0f81dbca77539dcc421cef922e34ceaa0
Gustaf Karlsson <gustaf1998@telia.com>
2022-10-12 12:19:44 +0200

    Added login page

---
+++ src/Pages/Login/Login.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Login = () =>{
+    return(
+        <>
+        <main>
+            <div  className="">
+                
+            </div>
+        </main>
+        </>
+    )
+}
