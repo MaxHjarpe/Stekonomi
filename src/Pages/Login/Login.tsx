b58373f35cc5f6acfac91f5e8482a7efb9f8e748
Gustaf Karlsson <gustaf1998@telia.com>
2022-10-12 15:36:22 +0200

    Lade till "Välkommen" i log in

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -7,13 +7,14 @@ const Login = () => {
       <main className="body-tag">
         <div className="box">
           <div className="form-box">
+          <h1>Välkommen</h1>
             <form action="#"></form>
             <input type="text" placeholder="Email" />
             <input type="password" placeholder="Lösenord" />
-            <button type="submit">Log In</button>
+            <button type="submit">Logga In</button>
             <div className="create-btn">
               <a href="login">
-                Create New Account
+                Skapa ett nytt konto
               </a>
             </div>
           </div>
