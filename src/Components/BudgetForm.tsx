e20386879c717a1d3428972ba3097ca16ceeea9b
AdamLundin <adam.lundin.syne21lin@tucsweden.se>
2022-10-13 12:42:36 +0200

    Added responsive elements to BudgetForm

--- src/Components/BudgetForm.tsx
+++ src/Components/BudgetForm.tsx
@@ -1,17 +1,19 @@
-import { Button } from "antd";
 import React from "react";
 import "../CSS/budgetForm.css";
 
 const BudgetForm = () => {
     return (
-        <div className="budget">
+        <>
             <h1 className="utgifter">Utgifter</h1>
-            <form>
-                <div className="budget__form">
-                    <div className="budget__form__left">
-                        <p className="income">Inkomst</p>
-                        <input type="number" required />
-                    </div>
+            <div className="budget">
+
+                <div className="budget__form__left">
+                    <p className="income">Inkomst</p>
+                    <input type="number" required />
+                </div>
+
+                <form className="budget__form">
+                    {/* <div> */}
                     <div className="budget__form__center">
                         <div className="formItem">
                             <label className="formItemTitle">Hyra</label>
@@ -26,24 +28,27 @@ const BudgetForm = () => {
                             <input id="transport" className="formItemInput" type="number" />
                         </div>
                         <div className="formItem">
-                            <label className="formItemTitle">Gym</label>
-                            <input id="gym" className="formItemInput" type="number" />
+                            <label className="formItemTitle">Övrigt</label>
+                            <input id="other" className="formItemInput" type="number" />
                         </div>
+                        <div className="budget__success">
+                            <button className="budget__button submit__button">Beräkna</button>
+                        </div>
+                        {/* </div> */}
                     </div>
-                    <div className="budget__form__right">
-                        <button 
-                        className="budget__button"
-                        //onClick={}
-                        >
-                            Historik
-                        </button>
-                    </div>
+                </form>
+
+                <div className="budget__form__right">
+                    <button
+                        className="budget__button history__button"
+                    //onClick={}
+                    >
+                        Historik
+                    </button>
                 </div>
-                <div className="budget__success">
-                    <button className="budget__button">Beräkna</button>
-                </div>
-            </form>
-        </div>
+
+            </div>
+        </>
     )
 }
 
