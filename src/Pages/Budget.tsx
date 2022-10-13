236112d8a3b9bd55044b9f862088ef196c9c0891
AdamLundin <adam.lundin.syne21lin@tucsweden.se>
2022-10-13 12:51:05 +0200

    BudgetPage "Date" to be in the middle

--- src/Pages/Budget.tsx
+++ src/Pages/Budget.tsx
@@ -1,10 +1,11 @@
 import React from "react"
 import BudgetForm from "../Components/BudgetForm";
+import "../CSS/budgetPage.css";
 
 const Budget = () => {
     return (
         <div>
-            <h1>Januari 2022</h1>
+            <h1 className="date">Januari 2022</h1>
             <BudgetForm/>
         </div>
     )
