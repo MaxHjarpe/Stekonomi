ff03e8a433924f0414859afe5d146cf8e9772997
Max Hjärpe <hjarpemax@gmail.com>
2022-10-13 18:11:47 +0200

    homepage styled

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import "../CSS/homepage.css";
-import homepageSvg from "../assets/working.svg";
+import homepageSvg from "../assets/homepageWave.svg";
+import background from "../assets/background.svg";
 
 const Homepage = () => {
-    return (
-        <div className="homepage">
-        <div className="container">
-            <img className="image" src={homepageSvg} alt="People in a hammock" />
+  return (
+    <>
+      <img className="backgroundImg" src={background} alt="" />
+      <div className="homepage">
+        <div className="image__container">
+          <img className="image" src={homepageSvg} alt="two people sitting on a car hood" />
         </div>
+        <div className="homepage__button__container">
+            <h1>Är du student?</h1>
+            <p className="text">Behöver du mer koll på din ekonomi? Skapa en budget hos oss!</p>
+          <button className="homepage__button">Skapa</button>
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
 
 export default Homepage;
