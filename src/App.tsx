3b68178b1287ec585c76a1eacf6a0e3d386291f0
Anton Karlsson <Anton7karlsson@gmail.com>
2022-10-13 11:43:25 +0200

    knappar utan funktion, ändra färger

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import logo from "./logo.svg";
+import Budgetspage from "./Pages/Budgetspage";
 import Homepage from "./Pages/Homepage";
+
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/budgets" element={<Budgetspage />} />
       </Routes>
     </>
   );
--- src/Components/Budgets.tsx
+++ src/Components/Budgets.tsx
@@ -2,11 +2,7 @@ import React from "react";
 
 const Budgets = () => {
     return (
-        <div className="BudgetButtons">
-            <button className="Button1">Namn, 2022-07-22</button>
-            <button className="Button2">Namn2, 2022-08-22</button>
-            <button className="Button3">Namn3, 2022-09-22</button>
-        </div>
+        <></>
     )
 }  
 
